011cdb5a09132ac0e7be0b7e7fcd0fb66a302064
Polina Titova <titova_polina@mail.ru>
2022-03-06 01:56:39 +0300

    index.js fixed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-
+import './js/ArrayBufferConverter';
+import './js/Character';
+import './js/Daemon';
+import './js/GetBuffer';
+import './js/Magician';
+import './js/MathCharacter';
+import './js/Zombie';
 import './css/style.css';
